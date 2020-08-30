3030481017dca85470f8b756f6dc13d3e1fe1b59
Ryan Tran <ryandtran@hotmail.com>
2020-08-29 18:12:12 -0700

    Refactor ListDropdown component

--- src/components/ListDropdown/ListDropdown.tsx
+++ src/components/ListDropdown/ListDropdown.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+import DropdownButton from 'react-bootstrap/DropdownButton'
 import DeleteListConfirmation from '../DeleteListConfirmation/DeleteListConfirmation';
+import Dropdown from 'react-bootstrap/esm/Dropdown';
 
 interface ListDropdownProps {
   listName: string,
@@ -9,39 +11,17 @@ interface ListDropdownProps {
 export default function ListDropdown(
   { listName, deleteList }: ListDropdownProps) {
   
-  const [showDropdown, setShowDropdown] = useState(false);
   const [showConfirmation, setShowConfirmation] = useState(false);
-
-  const openConfirmation = () => {
-    setShowDropdown(false);
-    setShowConfirmation(true);
-  };
   
   return (
-    <div className={`dropdown ${showDropdown ? 'show' : ''}`}>
-      <button className="btn btn-secondary dropdown-toggle"
-        type="button"
-        id="dropdownMenuButton"
-        data-toggle="dropdown"
-        aria-expanded={showDropdown}
-        onClick={() => setShowDropdown(!showDropdown)}
-        onBlur={(e: any) => {
-          if (!e.relatedTarget || e.relatedTarget.className !== 'dropdown-item') {
-            setShowDropdown(false);
-          }
-        }}
+    <DropdownButton id="dropdown-item-button"
+      title={<div className="three-dot-menu"></div>}
+    >
+      <Dropdown.Item as="button"
+        onClick={() => setShowConfirmation(true)}
       >
-        <div className="three-dot-menu"></div>
-      </button>
-      <div className={`dropdown-menu ${showDropdown ? 'show' : ''}`} 
-        aria-labelledby="dropdownMenuButton"
-      >
-        <button className="dropdown-item"
-          onClick={openConfirmation}
-        >
-          Delete
-        </button>
-      </div>
+        Delete
+      </Dropdown.Item>
 
       <DeleteListConfirmation
         show={showConfirmation}
@@ -49,6 +29,6 @@ export default function ListDropdown(
         deleteList={deleteList}
         handleClose={() => setShowConfirmation(false)}
       />
-    </div>
+    </DropdownButton>
   );
 }
