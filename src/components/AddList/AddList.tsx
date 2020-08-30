84f13301f3ae38e18eeea38fcdbebb4cea1d4fd1
LeoYulinLi <LeoYulinLi@gmail.com>
2020-08-29 18:37:19 -0700

    Added some AddList styling

--- src/components/AddList/AddList.tsx
+++ src/components/AddList/AddList.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import CardData from '../Types/CardData';
+import BCard from 'react-bootstrap/Card';
+import { Button, Form, InputGroup } from "react-bootstrap";
 
 interface AddListProps {
-    lists: Record<string, CardData[]>
-    setLists: React.Dispatch<React.SetStateAction<Record<string, CardData[]>>>
-    listOrder: string[]
-    setListOrder: React.Dispatch<React.SetStateAction<string[]>>
+  lists: Record<string, CardData[]>
+  setLists: React.Dispatch<React.SetStateAction<Record<string, CardData[]>>>
+  listOrder: string[]
+  setListOrder: React.Dispatch<React.SetStateAction<string[]>>
 }
 
 export default function AddList({ lists, setLists, listOrder, setListOrder }: AddListProps) {
@@ -23,31 +25,33 @@ export default function AddList({ lists, setLists, listOrder, setListOrder }: Ad
     setListName('');
   };
 
-  return (
-    <>
-      <div onClick={() => setAddButton(true)}>
-        <div
-          style={{
-            display: addButton ? "none" : "inline-block"
-          }}
-        >
-          + Add a new list
-        </div>
+  function cardBody() {
+    if (addButton) {
+      return <>
+        <BCard.Header>
+          <Form onSubmit={ addList }>
+            <InputGroup>
+              <Form.Control type="text"
+                            value={ listName }
+                            onChange={ updateListName }
+                            required
+                            autoFocus
+              />
+              <InputGroup.Append>
+                <Button type="submit">Create</Button>
+              </InputGroup.Append>
+            </InputGroup>
+          </Form>
+        </BCard.Header>
+        </>
+    } else {
+      return <BCard.Body className="d-flex align-items-center justify-content-center">
+        <Button onClick={ () => setAddButton(true) } variant="link">Add a new list</Button>
+      </BCard.Body>
+    }
+  }
 
-        <form
-          style={{
-            display: addButton ? "inline-block" : "none"
-          }}
-          onSubmit = {addList}
-        >
-          <input type="text"
-            value={listName}
-            onChange={updateListName}
-            required
-          />
-          <button>Add List</button>
-        </form>
-      </div>
-    </>
-  );
+  return <BCard className="list-card">
+    { cardBody() }
+  </BCard>
 }
