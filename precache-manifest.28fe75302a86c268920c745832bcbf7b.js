0b77a4915c4be5f92ce798c791db4253980d8a99
bvsillorequez <60529554+bvsillorequez@users.noreply.github.com>
2020-08-30 23:53:17 +0000

    Deploying to gh-pages from  @ 391f3677a70589cc01a4b89127e44919d9db802d 🚀

---
+++ precache-manifest.28fe75302a86c268920c745832bcbf7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b93b99d56868b3358b59550285ea58c3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "10ffd6d65ffb5172f7cd",
+    "url": "./static/css/main.ddee65be.chunk.css"
+  },
+  {
+    "revision": "9c856a8751259ebdad1a",
+    "url": "./static/js/2.5c78b3ad.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.5c78b3ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10ffd6d65ffb5172f7cd",
+    "url": "./static/js/main.c85ae3db.chunk.js"
+  },
+  {
+    "revision": "08faba5d4c1e8245f5ee",
+    "url": "./static/js/runtime-main.701a4cce.js"
+  }
+]);
